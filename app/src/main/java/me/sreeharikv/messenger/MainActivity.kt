cc3be039cb86d82fa080f0cde673960104e2f74a
SREEHARI K.V <sreeharivijayan619@gmail.com>
2018-07-29 00:23:16 +0530

    registration successfull

--- app/src/main/java/me/sreeharikv/messenger/MainActivity.kt
+++ app/src/main/java/me/sreeharikv/messenger/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -17,13 +19,7 @@ class MainActivity : AppCompatActivity() {
 
         register_button_register.setOnClickListener {
 
-            val email = email_edittext_reg.text.toString()
-            val password = password_edittext_reg.text.toString()
-            val username = username_edittext_reg.text.toString()
-
-            Log.d("MainActivity", "email is "+email)
-            Log.d("MainActivity", "password  is "+password)
-            Log.d("MainActivity", "username is "+username)
+            perfromaction()
 
         }
 
@@ -36,5 +32,33 @@ class MainActivity : AppCompatActivity() {
         }
 
 
+    }
+
+    private fun perfromaction(){
+        val email = email_edittext_reg.text.toString()
+        val password = password_edittext_reg.text.toString()
+        val username = username_edittext_reg.text.toString()
+
+        Log.d("MainActivity", "email is "+email)
+        Log.d("MainActivity", "password  is "+password)
+        Log.d("MainActivity", "username is "+username)
+
+        if(email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "please enter the credentials", Toast.LENGTH_LONG).show()
+            return
+        }
+
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener {
+                    if(!it.isSuccessful) return@addOnCompleteListener
+
+                    // else if
+                    Log.d("Main", "The user registered successfull with uid"+{it.result.user.uid})
+
+                }
+                .addOnFailureListener {
+                    Log.d("Main", "Failed to register the user")
+                    Toast.makeText(this, "Registration failed : ${it.message}", Toast.LENGTH_LONG).show()
+                }
     }
 }
