72283d6727d7e545b478ffefdad80fe011dcfbc4
SREEHARI K.V <sreeharivijayan619@gmail.com>
2018-08-04 13:09:31 +0530

    saved user to db and issue with sign in btn fixed

--- app/src/main/java/me/sreeharikv/messenger/MainActivity.kt
+++ app/src/main/java/me/sreeharikv/messenger/MainActivity.kt
@@ -3,13 +3,18 @@ package me.sreeharikv.messenger
 import android.app.Activity
 import android.content.Intent
 import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import android.util.Log
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
 
@@ -26,7 +31,7 @@ class MainActivity : AppCompatActivity() {
 
         }
 
-        signintextview.setOnClickListener {
+        already_have_account.setOnClickListener {
             Log.d("MainActivity", "Try to show the login page")
 
             val intent = Intent(this, LoginActivity::class.java)
@@ -45,13 +50,15 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    var selectedphotouri : Uri? = null
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
         if(resultCode == Activity.RESULT_OK && requestCode == 0 && data != null){
             //photo selected
-            val uri = data.data
-            val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, uri)
+            selectedphotouri = data.data
+            val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, selectedphotouri)
             val bitmapDrawable = BitmapDrawable(bitmap)
 
             photo_button.setBackgroundDrawable(bitmapDrawable)
@@ -78,12 +85,47 @@ class MainActivity : AppCompatActivity() {
                     if(!it.isSuccessful) return@addOnCompleteListener
 
                     // else if
-                    Log.d("Main", "The user registered successfull with uid"+{it.result.user.uid})
+                    Log.d("MainActivity", "The user registered successfull with uid"+{it.result.user.uid})
+                    uploadimagetofirebasestorage()
+
 
                 }
                 .addOnFailureListener {
-                    Log.d("Main", "Failed to register the user")
+                    Log.d("MainActivity", "Failed to register the user")
                     Toast.makeText(this, "Registration failed : ${it.message}", Toast.LENGTH_LONG).show()
                 }
     }
+    private fun uploadimagetofirebasestorage(){
+
+        if(selectedphotouri == null) return
+
+        val filename = UUID.randomUUID().toString()
+        val ref = FirebaseStorage.getInstance().getReference("/images/$filename")
+
+        ref.putFile(selectedphotouri!!)
+
+                .addOnSuccessListener {
+                    Log.d("MainActivity", "photo uploaded successfully")
+
+                    ref.downloadUrl.addOnSuccessListener {
+                        saveusertofirebasedatabase(it.toString())
+                    }
+                }
+
+
+    }
+    private fun saveusertofirebasedatabase(profileimageurl : String){
+
+        val uid = FirebaseAuth.getInstance().uid
+        val ref = FirebaseDatabase.getInstance().getReference("/users/$uid")
+
+        val user = User(uid.toString(),username_edittext_reg.text.toString(),profileimageurl)
+        ref.setValue(user)
+        //save entire object
+
+        Log.d("MainActivity","Successfully saved user to database")
+
+    }
 }
+
+class User(val uid : String,val Username : String,val profileimageurl: String)
